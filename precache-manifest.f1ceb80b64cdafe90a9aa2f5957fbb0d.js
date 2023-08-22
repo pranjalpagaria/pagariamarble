f78478f2cc999e5e5459baf61e99f1aa4f18ad30
pranjalpagaria <80514293+pranjalpagaria@users.noreply.github.com>
2023-08-22 22:49:26 +0530

    Updates

---
+++ precache-manifest.f1ceb80b64cdafe90a9aa2f5957fbb0d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "97962287b715e09d54b1",
+    "url": "/pagariamarble/static/css/main.b72ebce2.chunk.css"
+  },
+  {
+    "revision": "97962287b715e09d54b1",
+    "url": "/pagariamarble/static/js/main.b168a241.chunk.js"
+  },
+  {
+    "revision": "3d756b0f8c613be35de6",
+    "url": "/pagariamarble/static/js/runtime~main.e95d8e14.js"
+  },
+  {
+    "revision": "2498e35610acf890bad1",
+    "url": "/pagariamarble/static/css/2.c778775f.chunk.css"
+  },
+  {
+    "revision": "2498e35610acf890bad1",
+    "url": "/pagariamarble/static/js/2.dfaf1d26.chunk.js"
+  },
+  {
+    "revision": "0a78b2b425ce262d37b0",
+    "url": "/pagariamarble/static/js/3.f2d91817.chunk.js"
+  },
+  {
+    "revision": "089476795109b2d31fe09dff052f1faa",
+    "url": "/pagariamarble/static/media/pwhite.08947679.jpeg"
+  },
+  {
+    "revision": "b89571782b23825f042f7ed02b766806",
+    "url": "/pagariamarble/static/media/pinkmarble.b8957178.jpeg"
+  },
+  {
+    "revision": "7af6a92abd2d56276d76b0a822db7e14",
+    "url": "/pagariamarble/static/media/MajesticBlack.7af6a92a.jpg"
+  },
+  {
+    "revision": "9e462b0e3e4422211afeacd5305c62f8",
+    "url": "/pagariamarble/static/media/jhanjhar.9e462b0e.jpg"
+  },
+  {
+    "revision": "f9b87f9bb0e109637111f0c46927e8fa",
+    "url": "/pagariamarble/static/media/greenmarble.f9b87f9b.jpg"
+  },
+  {
+    "revision": "5f6846538b239f3a74a61318bcffc5c9",
+    "url": "/pagariamarble/static/media/fishblack.5f684653.jpg"
+  },
+  {
+    "revision": "8e3f6627fccef2f8bbc0e966a4ec6ce5",
+    "url": "/pagariamarble/static/media/PEBBLE1.8e3f6627.jpg"
+  },
+  {
+    "revision": "c3d5e43fc360af09a173e7ddd8203a12",
+    "url": "/pagariamarble/static/media/PEBBLE2.c3d5e43f.jpg"
+  },
+  {
+    "revision": "bf9d3cfa8e30da47d24adbabdfeb90d2",
+    "url": "/pagariamarble/static/media/quartz2.bf9d3cfa.jpg"
+  },
+  {
+    "revision": "5c7773a823aa973ff55ed1f7bd89ca6b",
+    "url": "/pagariamarble/static/media/quartz6.5c7773a8.jpeg"
+  },
+  {
+    "revision": "8e3f6627fccef2f8bbc0e966a4ec6ce5",
+    "url": "/pagariamarble/static/media/pebbles_1.8e3f6627.jpg"
+  },
+  {
+    "revision": "efbdef990cd9fdad2f50fdb132b26e34",
+    "url": "/pagariamarble/static/media/1.efbdef99.jpg"
+  },
+  {
+    "revision": "13ad9af10b9e65ef1ccc6b8111bc5225",
+    "url": "/pagariamarble/static/media/2.13ad9af1.jpg"
+  },
+  {
+    "revision": "de289da88636894b5d8c346aac8dcea4",
+    "url": "/pagariamarble/static/media/contactimage.de289da8.png"
+  },
+  {
+    "revision": "c09ed92f3ab2ee1b9ba8dd3bc7f39dce",
+    "url": "/pagariamarble/static/media/IM4.c09ed92f.png"
+  },
+  {
+    "revision": "29b0ec948ee7d6996ae7194808f815ae",
+    "url": "/pagariamarble/static/media/jkbrown.29b0ec94.jpg"
+  },
+  {
+    "revision": "0535454f1bf7f45204b533a9464f2fee",
+    "url": "/pagariamarble/static/media/Morwadwhite.0535454f.jpeg"
+  },
+  {
+    "revision": "b149b5bc4c96441fd408a95fe93b2e58",
+    "url": "/pagariamarble/static/media/IG1.b149b5bc.png"
+  },
+  {
+    "revision": "1aa91d4f110657c6fb37e2cd76183a09",
+    "url": "/pagariamarble/static/media/brazilbrowngranite.1aa91d4f.jpg"
+  },
+  {
+    "revision": "625e4e5e82450ce90f76cfb602bc8259",
+    "url": "/pagariamarble/static/media/lakhared.625e4e5e.jpg"
+  },
+  {
+    "revision": "4237dd44408eaa96c10d478d8fc4070e",
+    "url": "/pagariamarble/static/media/lapatrakotada.4237dd44.jpeg"
+  },
+  {
+    "revision": "4fd2f330acda801dcea128f7885f3447",
+    "url": "/pagariamarble/static/media/Pebbles0.4fd2f330.png"
+  },
+  {
+    "revision": "cc7c4c826c03ec0bea715ea5f28267ae",
+    "url": "/pagariamarble/static/media/PEBBLE3.cc7c4c82.jpg"
+  },
+  {
+    "revision": "753cb4d0861135a14a065463067ae955",
+    "url": "/pagariamarble/static/media/PEBBLE5.753cb4d0.jpg"
+  },
+  {
+    "revision": "91afdf1b6ae140a712801595d7c493b7",
+    "url": "/pagariamarble/static/media/IQ4.91afdf1b.png"
+  },
+  {
+    "revision": "da3a6b1dc5c69023664fd05b4ffba7dc",
+    "url": "/pagariamarble/static/media/quartz1.da3a6b1d.jpg"
+  },
+  {
+    "revision": "8764ccebfa07fc69b46dd21be3877e6f",
+    "url": "/pagariamarble/static/media/quartz4.8764cceb.jpg"
+  },
+  {
+    "revision": "00e6c4f9518f01e35c679cb1a323fb2f",
+    "url": "/pagariamarble/static/media/quartz5.00e6c4f9.jpg"
+  },
+  {
+    "revision": "d9b591ef57c566fd6dff340bc9b20a93",
+    "url": "/pagariamarble/static/media/quartz6.d9b591ef.jpg"
+  },
+  {
+    "revision": "a3be3e0994b1d33049bced9a1e6eac64",
+    "url": "/pagariamarble/index.html"
+  }
+];
