c853a6ecf4a57d7ce681064a827506a17278e55d
pranjalpagaria <80514293+pranjalpagaria@users.noreply.github.com>
2023-08-25 15:42:42 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pagariamarble/precache-manifest.51274d020dc7335edc8e139ea6eb6535.js"
+  "/pagariamarble/precache-manifest.1fd8b4388defae36c6474e430faa0405.js"
 );
 
 workbox.clientsClaim();
